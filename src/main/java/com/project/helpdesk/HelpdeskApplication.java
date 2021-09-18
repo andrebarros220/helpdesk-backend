6a8d1c46f306f0d604e5943496f0ce44ca91ace8
andrebarros220 <andrebarros220@gmail.com>
2021-09-18 09:48:45 -0300

    carga inicial de dados

--- src/main/java/com/project/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/project/helpdesk/HelpdeskApplication.java
@@ -1,13 +1,54 @@
 package com.project.helpdesk;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class HelpdeskApplication {
+import com.project.helpdesk.domain.Chamado;
+import com.project.helpdesk.domain.Cliente;
+import com.project.helpdesk.domain.Tecnico;
+import com.project.helpdesk.domain.enums.Perfil;
+import com.project.helpdesk.domain.enums.Prioridade;
+import com.project.helpdesk.domain.enums.Status;
+import com.project.helpdesk.repositories.ChamadoRepository;
+import com.project.helpdesk.repositories.ClienteRepository;
+import com.project.helpdesk.repositories.TecnicoRepository;
 
+import ch.qos.logback.core.status.StatusListenerAsList;
+
+@SpringBootApplication
+public class HelpdeskApplication implements CommandLineRunner {
+
+	
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Tecnico tec1 = new Tecnico (null, "andre barros","331408728877", "andre@email.com", "123");
+		tec1.addPerfil(Perfil.ADMIN);
+		
+		Cliente cli1 = new Cliente(null, "Linus Torvalds", "80527954780", "torvalds@mail.co", "1234" );
+		
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
+		
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
+	}
+
+	
+	
 }
