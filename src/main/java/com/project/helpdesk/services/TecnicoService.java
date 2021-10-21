7456951dbd183e1598fa058a8eb8ac3fa045ef0a
andrebarros220 <andrebarros220@gmail.com>
2021-10-21 07:18:09 -0300

    Aula 12 - objeto não encontrado

--- src/main/java/com/project/helpdesk/services/TecnicoService.java
+++ src/main/java/com/project/helpdesk/services/TecnicoService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.project.helpdesk.domain.Tecnico;
 import com.project.helpdesk.repositories.TecnicoRepository;
+import com.project.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -14,7 +15,7 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado ! Id: " + id) );
 	}
 
 }
